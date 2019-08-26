0e4f7bb2e651be9136f00c63d4cb33003aaebcb3
vaibhav <da-vaibhav@users.noreply.github.com>
2019-08-26 15:27:41 +0530

    clean

--- src/App.jsx
+++ src/App.jsx
@@ -4,32 +4,15 @@ import FunctionChild  from "./fn-child";
 
 class App extends Component {
   state = {
-    childCount: 3,
-    randomNum: Math.random(),
   };
 
   render() {
-    const { childCount, randomNum } = this.state;
     return (
       <div>
-        <span>we are rendering {childCount} child components</span>
-        <FunctionChild num={randomNum} />
-        <hr/>
-        <FunctionChild num={randomNum} clickEvent={this.updateRandomNum}/>
-
-         {(childCount < 3) ? "Less than 3" : "sufficient count" }
-
-        <hr/>
-        <button onClick={this.updateRandomNum}>update random number</button>
+        <FunctionChild />
         <hr/>
       </div>
     );
-  }
-
-  updateRandomNum = () => {
-    this.setState({randomNum: Math.random()}, () => {
-      console.log('random number updated!');
-    });
   }
 }
 
